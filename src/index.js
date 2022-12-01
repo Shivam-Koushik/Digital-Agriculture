36a6d81d2c5dcf7a0ea88d0882dbf0688eaab32b
Shivam-Koushik <93977658+ShivamKoushik@users.noreply.github.com>
2022-12-01 12:58:01 +0530

    updated

--- src/index.js
+++ src/index.js
@@ -5,6 +5,24 @@ const app = express()
 const route = require("./routes/route")
 require("dotenv").config()
 
+//Authencation portion using google=================================================
+const passport = require("passport")
+
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+ 
+passport.use(new GoogleStrategy({
+    clientID: `${process.env.clientID}`,
+    clientSecret: `${process.env.clientSecret}`,
+    callbackURL: "http://localhost:3000"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+      return cb(null, profile); 
+  }
+));
+
+app.set('view engine','ejs')
+//====================================================================================
+
 app.use(bodyParse.json())
 
 mongoose.connect(`mongodb+srv://ShivamKoushik:${process.env.cluster_Password}@cluster0.k1qkf.mongodb.net/${process.env.cluster_Name}?retryWrites=true&w=majority`, {
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,10 +1,21 @@
 const express = require("express")
 const route = express.Router()
+const passport = require("passport")
 const signUpControllers = require("../controller/register")
 const cropContrllers = require("../controller/crop")
 const middlewareController = require("../middleware/middleware")
 
 //____________________________Admin
+route.get("/",(req,res)=>res.render('index'))
+route.get('/google',
+  passport.authenticate('google', { scope: ['profile'] }));
+ 
+route.get('/auth/google/callback', 
+  passport.authenticate('google', { failureRedirect: '/' }),
+  (req, res) =>{
+    res.render('/');
+  });
+
 route.post("/signUp",signUpControllers.signUp)
 route.post("/login",signUpControllers.login)
 route.post("/createCrop/:userId",cropContrllers.createCrop)
